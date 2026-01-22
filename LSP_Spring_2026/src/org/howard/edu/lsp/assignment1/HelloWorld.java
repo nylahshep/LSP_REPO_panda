a1c2b42e415625edf51ddef9f536ef296b7592a3
nylahsierra <nylahsierra@10.160.8.145>
2026-01-21 20:19:22 -0500

    This is my first commit.

---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	
+	/**
+	 * Name: Nylah Shepherd
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
